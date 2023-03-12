387bf088d6de939dde29334383237b1bbb56887e
Devashish Choudhary <93133135+DevashishCh@users.noreply.github.com>
2023-03-13 00:52:09 +0530

    Time: 3 ms (14.47%), Space: 42.7 MB (35.10%) - LeetHub

---
+++ 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        Map<Integer, List<Integer>> map = new TreeMap<>(Collections.reverseOrder());
+        traverseBST(root, map, 0);
+        List<List<Integer>> result = new ArrayList<>();
+        for (int key : map.keySet()) result.add(map.get(key));
+        return result;
+    }
+
+    private void traverseBST(TreeNode node, Map<Integer, List<Integer>> map, int lvl) {
+        if (node == null) return;
+
+        lvl++;
+
+        traverseBST(node.left, map, lvl);
+
+        if (map.containsKey(lvl)) {
+            map.get(lvl).add(node.val);
+        } else {
+            List<Integer> list = new ArrayList<>();
+            list.add(node.val);
+            map.put(lvl, list);
+        }
+
+        traverseBST(node.right, map, lvl);
+    }
+}
